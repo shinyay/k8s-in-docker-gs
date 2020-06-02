652a6c59c90e6403fd45fecd9cc723989eb1847f
shinyay <shinya.com@gmail.com>
2020-06-03 07:50:38 +0900

    Add ingress nginx deployment

---
+++ deploy-ingress-nginx.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/env fish
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/static/provider/kind/deploy.yaml
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=120s
